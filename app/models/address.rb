3236ca0554374926a9fb90338957f904dbd7069a
Fabio Maiolo <fabio_maiolo@hotmail.com>
2021-05-05 22:21:18 -0300

    shipping and address validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,16 @@
 class Address < ApplicationRecord
+  has_many :shippings
+
+  validates :address_line, presence: true
+  validates :street_number, presence: true
+  validates :street_name, presence: true
+  validates :comments, presence: true
+  validates :zipcode, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :country, presence: true
+  validates :neighborhood, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :receiver_phone, presence: true
 end
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,4 +1,10 @@
 class Shipping < ApplicationRecord
   belongs_to :order
   belongs_to :address
+
+  validates_associated :address
+  validates :shipment_type, presence: true
+  validates :date_created, presence: true
+  validates :status, presence: true
+  validates :address, presence: true
 end
