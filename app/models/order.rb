20b78da12b3207ece7a07444a82253997a09d24f
Fabio Maiolo <fabio_maiolo@hotmail.com>
2021-05-05 21:07:58 -0300

    order validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,16 @@ class Order < ApplicationRecord
   has_one :shipping
   belongs_to :buyer
   
+
+  validates_associated :payments
+  validates_associated :orders_items
+  validates_associated :items
+  validates_associated :shipping
+  validates_associated :buyer
+  validates :total_amount, presence: true
+  validates :total_shipping, presence: true
+  validates :total_amount_with_shipping, presence: true
+  validates :status, presence: true
+  validates :date_created, presence: true
+  validates :expiration_date, presence: true 
 end
