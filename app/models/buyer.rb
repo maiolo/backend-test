0978cd035e68a31fa7b49b40774832785c80c619
Fabio Maiolo <fabio_maiolo@hotmail.com>
2021-05-05 22:37:37 -0300

    all validations done

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -3,4 +3,9 @@ class Buyer < ApplicationRecord
   has_many :billing_infos
   has_many :orders
   has_many :payments
+
+  validates :nickname, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,3 +1,7 @@
 class Phone < ApplicationRecord
   belongs_to :buyer
+
+  validates :buyer_id, presence: true
+  validates :area_code, presence: true
+  validates :number, presence: true
 end
