eaa512ad5222d3c5a8fc6f927c313c7ccf5f645c
Fabio Maiolo <fabio_maiolo@hotmail.com>
2021-05-06 23:00:35 -0300

    just updating the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_015236) do
+ActiveRecord::Schema.define(version: 2021_05_02_220524) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "address_line"
