80d70d8e4b50f44de9b528e843a93f434043827e
Fabio Maiolo <fabio_maiolo@hotmail.com>
2021-05-06 22:55:19 -0300

    some refactoring on the payload model

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -1,2 +1,9 @@
-class Payload < ApplicationRecord
+class Payload
+  include ActiveModel::Model
+
+  attr_accessor :json_url
+
+  def persisted?
+    true
+  end
 end
--- db/migrate/20210502232856_create_payloads.rb
+++
@@ -1,9 +0,0 @@
-class CreatePayloads < ActiveRecord::Migration[6.0]
-  def change
-    create_table :payloads do |t|
-      t.string :json_url
-
-      t.timestamps
-    end
-  end
-end
